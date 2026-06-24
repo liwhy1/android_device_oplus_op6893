5a56d71c113c28011d78da59cacbbe3146262a0f
Frost <frost.github@proton.me>
2026-06-24 14:29:59 +0530

    op6893: Rename libmnl to libmnl-v30
    Change-Id: Ia609016bc654c6c54651c91446b0ebb4032fcef4
    Signed-off-by: Frost <frost.github@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -77,6 +77,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libstagefright_foundation-v33.so'),
     'vendor/bin/hw/mtkfusionrild' : blob_fixup()
         .add_needed('libutils-v32.so'),
+    'vendor/bin/mnld': blob_fixup()
+        .replace_needed('libmnl.so', 'libmnl-v30.so'),
     'vendor/bin/mtk_agpsd': blob_fixup()
         .replace_needed('libcrypto.so', 'libcrypto-v32.so')
         .replace_needed('libssl.so', 'libssl-v32.so'),
